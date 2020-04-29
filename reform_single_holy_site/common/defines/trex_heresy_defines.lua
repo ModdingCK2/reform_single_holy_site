5a5c253e26c1345dbe778c6a3523f37bf5ee9489
Kevin van Rijn <kevinvanrijn@users.noreply.github.com>
2020-04-29 09:59:26 +0200

    Update defines to allow reformation with a single holy site
    But with a large piety cost and high authority requirement.

---
+++ reform_single_holy_site/common/defines/trex_heresy_defines.lua
@@ -0,0 +1,8 @@
+-- Format for overwriting define values:
+--
+-- NDefines.NReligion.RETINUE_FROM_REALMSIZE = 3.0
+-- NDefines.NMilitary.RETINUE_REINFORCE_COST = 2.5
+
+NDefines.NReligion.REFORM_RELIGION_MIN_AUTHORITY = 0.6
+NDefines.NReligion.REFORM_RELIGION_MIN_HOLY_SITES = 1
+NDefines.NReligion.REFORM_RELIGION_PIETY_COST = 20000
